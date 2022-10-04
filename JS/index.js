1e2eb42d832d37419ac62e50992f831566a1a881
Aureliu-C <aureliu.cojocaru130@gmail.com>
2022-10-04 16:17:32 +0200

    Basic hw

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+let Name = "Aureliu";
+let lastName = "Cojocaru";
+let Space = " ";
+console.log(Name + Space + lastName);
+let Advise = ".Make sure to use the variables!";
+console.log("Hello, my name is", Name + Space + lastName + Space + Advise);
+let Age = 28;
+let curentYear = 2022;
+console.log("Birth year", curentYear - Age);
